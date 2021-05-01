8b7174a707733729b7f13c7a8d3919f309668660
Joseph Woolley <joe@calledtoconstruct.net>
2021-05-01 12:00:14 -0400

    Improve performance of line counter.

--- Scripts/Editor/PropertyDrawers/ResizableTextAreaPropertyDrawer.cs
+++ Scripts/Editor/PropertyDrawers/ResizableTextAreaPropertyDrawer.cs
@@ -2,120 +2,135 @@
 using UnityEditor;
 using System.Text.RegularExpressions;
 using System;
+using System.Linq;
 
 namespace NaughtyAttributes.Editor
 {
-	[CustomPropertyDrawer(typeof(ResizableTextAreaAttribute))]
-	public class ResizableTextAreaPropertyDrawer : PropertyDrawerBase
-	{
-		private Func<string, float> calculateMaximumTextAreaHeight;
-		private float? minimum;
-		private float? maximum;
+    [CustomPropertyDrawer(typeof(ResizableTextAreaAttribute))]
+    public class ResizableTextAreaPropertyDrawer : PropertyDrawerBase
+    {
+        private static readonly string[] delimiters = new[] { "\r\n", "\n\r", "\r", "\n", Environment.NewLine };
+        private Func<string, float> calculateMaximumTextAreaHeight;
+        private float? minimum;
+        private float? maximum;
 
-		public ResizableTextAreaPropertyDrawer()
-		{
-			calculateMaximumTextAreaHeight = InitializeMaximumHeightCalculator;
-		}
+        public ResizableTextAreaPropertyDrawer()
+        {
+            calculateMaximumTextAreaHeight = InitializeMaximumHeightCalculator;
+        }
 
-		protected override float GetPropertyHeight_Internal(SerializedProperty property, GUIContent label)
-		{
-			if (property.propertyType == SerializedPropertyType.String)
-			{
-				float labelHeight = EditorGUIUtility.singleLineHeight;
-				float textAreaHeight = Math.Min(
-					Math.Max(GetTextAreaHeight(property.stringValue), GetMinimumTextAreaHeight()),
-					GetMaximumTextAreaHeight(property.stringValue)
-				);
-				return labelHeight + textAreaHeight;
+        protected override float GetPropertyHeight_Internal(SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType == SerializedPropertyType.String)
+            {
+                float labelHeight = EditorGUIUtility.singleLineHeight;
+                float textAreaHeight = Math.Min(
+                    Math.Max(GetTextAreaHeight(property.stringValue), GetMinimumTextAreaHeight()),
+                    GetMaximumTextAreaHeight(property.stringValue)
+                );
+                return labelHeight + textAreaHeight;
+            }
+            else
+            {
+                return GetPropertyHeight(property) + GetHelpBoxHeight();
+            }
+        }
+
+        protected override void OnGUI_Internal(Rect rect, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(rect, label, property);
+
+            if (property.propertyType == SerializedPropertyType.String)
+            {
+                Rect labelRect = new Rect()
+                {
+                    x = rect.x,
+                    y = rect.y,
+                    width = rect.width,
+                    height = EditorGUIUtility.singleLineHeight
+                };
+
+                EditorGUI.LabelField(labelRect, label.text);
+
+                EditorGUI.BeginChangeCheck();
+
+                Rect textAreaRect = new Rect()
+                {
+                    x = labelRect.x,
+                    y = labelRect.y + EditorGUIUtility.singleLineHeight,
+                    width = labelRect.width,
+                    height = Math.Min(
+                        Math.Max(GetTextAreaHeight(property.stringValue), GetMinimumTextAreaHeight()),
+                        GetMaximumTextAreaHeight(property.stringValue)
+                    )
+                };
+
+                string textAreaValue = EditorGUI.TextArea(textAreaRect, property.stringValue);
+
+                if (EditorGUI.EndChangeCheck())
+                {
+                    property.stringValue = textAreaValue;
+                }
+            }
+            else
+            {
+                string message = typeof(ResizableTextAreaAttribute).Name + " can only be used on string fields";
+                DrawDefaultPropertyAndHelpBox(rect, property, message, MessageType.Warning);
+            }
+
+            EditorGUI.EndProperty();
+        }
+
+        private int GetNumberOfLines(string text)
+        {
+            return delimiters
+				.AsParallel()
+				.Select(delimiter => CountOccurrences(text, delimiter))
+				.Sum() + 1;
+        }
+
+        private int CountOccurrences(string text, string delimiter)
+        {
+			var count = 0;
+			var position = text.IndexOf(delimiter, 0);
+			while (position >= 0) {
+				++count;
+				position = text.IndexOf(delimiter, position + 1);
 			}
-			else
-			{
-				return GetPropertyHeight(property) + GetHelpBoxHeight();
-			}
-		}
+			return count;
+        }
 
-		protected override void OnGUI_Internal(Rect rect, SerializedProperty property, GUIContent label)
-		{
-			EditorGUI.BeginProperty(rect, label, property);
+        private float GetMinimumTextAreaHeight()
+        {
+            minimum = minimum.HasValue
+                ? minimum
+                : (EditorGUIUtility.singleLineHeight - 3.0f) * ((ResizableTextAreaAttribute)attribute).MinimumLines + 3.0f;
 
-			if (property.propertyType == SerializedPropertyType.String)
-			{
-				Rect labelRect = new Rect()
-				{
-					x = rect.x,
-					y = rect.y,
-					width = rect.width,
-					height = EditorGUIUtility.singleLineHeight
-				};
+            return minimum.Value;
+        }
 
-				EditorGUI.LabelField(labelRect, label.text);
+        private float InitializeMaximumHeightCalculator(string text)
+        {
+            var targetAttribute = (ResizableTextAreaAttribute)attribute;
+            calculateMaximumTextAreaHeight = (targetAttribute.MaximumLines > 0)
+                ? (Func<string, float>)GetMaximumTextAreaHeight
+                : (Func<string, float>)GetTextAreaHeight;
+            return calculateMaximumTextAreaHeight(text);
+        }
 
-				EditorGUI.BeginChangeCheck();
+        private float GetMaximumTextAreaHeight(string _)
+        {
+            maximum = maximum.HasValue
+                ? maximum
+                : (EditorGUIUtility.singleLineHeight - 3.0f) * ((ResizableTextAreaAttribute)attribute).MaximumLines + 3.0f;
 
-				Rect textAreaRect = new Rect()
-				{
-					x = labelRect.x,
-					y = labelRect.y + EditorGUIUtility.singleLineHeight,
-					width = labelRect.width,
-					height = Math.Min(
-						Math.Max(GetTextAreaHeight(property.stringValue), GetMinimumTextAreaHeight()),
-						GetMaximumTextAreaHeight(property.stringValue)
-					)
-				};
+            return maximum.Value;
+        }
 
-				string textAreaValue = EditorGUI.TextArea(textAreaRect, property.stringValue);
-
-				if (EditorGUI.EndChangeCheck())
-				{
-					property.stringValue = textAreaValue;
-				}
-			}
-			else
-			{
-				string message = typeof(ResizableTextAreaAttribute).Name + " can only be used on string fields";
-				DrawDefaultPropertyAndHelpBox(rect, property, message, MessageType.Warning);
-			}
-
-			EditorGUI.EndProperty();
-		}
-
-		private int GetNumberOfLines(string text)
-		{
-			string content = Regex.Replace(text, @"\r\n|\n\r|\r|\n", Environment.NewLine);
-			string[] lines = content.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
-			return lines.Length;
-		}
-
-		private float GetMinimumTextAreaHeight()
-		{
-			minimum = minimum.HasValue
-				? minimum
-				: (EditorGUIUtility.singleLineHeight - 3.0f) * ((ResizableTextAreaAttribute)attribute).MinimumLines + 3.0f;
-
-			return minimum.Value;
-		}
-
-		private float InitializeMaximumHeightCalculator(string text) {
-			var targetAttribute = (ResizableTextAreaAttribute) attribute;
-			calculateMaximumTextAreaHeight = (targetAttribute.MaximumLines > 0)
-				? (Func<string, float>)GetMaximumTextAreaHeight
-				: (Func<string, float>)GetTextAreaHeight;
-			return calculateMaximumTextAreaHeight(text);
-		}
-
-		private float GetMaximumTextAreaHeight(string _)
-		{
-			maximum = maximum.HasValue
-				? maximum
-				: (EditorGUIUtility.singleLineHeight - 3.0f) * ((ResizableTextAreaAttribute)attribute).MaximumLines + 3.0f;
-
-			return maximum.Value;
-		}
-
-		private float GetTextAreaHeight(string text)
-		{
-			float height = (EditorGUIUtility.singleLineHeight - 3.0f) * GetNumberOfLines(text) + 3.0f;
-			return height;
-		}
-	}
+        private float GetTextAreaHeight(string text)
+        {
+            float height = (EditorGUIUtility.singleLineHeight - 3.0f) * GetNumberOfLines(text) + 3.0f;
+            return height;
+        }
+    }
 }
